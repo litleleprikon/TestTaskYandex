a2b979dba1567cf1cd23ef128c1d28ee86cf334b
Emil <litleleprikon@gmail.com>
2014-04-09 22:04:44 +0400

    Initial commit

---
+++ clock_angle/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'litleleprikon'
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as ex:
+        print("Keyboard interrupt")
---
+++ clock_angle/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'litleleprikon'
+
+
+class TimeParser:
+
+    def __init__(self):
+        pass
+
+
+class Mathematician:
+
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as ex:
+        print("Keyboard interrupt")
