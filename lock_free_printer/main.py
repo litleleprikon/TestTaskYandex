e20e8a19f25948083da9e4b26519bc6bb63e310e
Emil <litleleprikon@gmail.com>
2014-04-30 17:36:34 +0400

    test on linux lock free printer #2

--- lock_free_printer/main.py
+++ lock_free_printer/main.py
@@ -4,7 +4,6 @@ __author__ = 'litleleprikon'
 
 import re
 from multiprocessing import Process, Lock
-from functools import partial
 from time import sleep
 
 
@@ -19,15 +18,16 @@ def main():
         query_parser = re.compile(r"(\d+(.\d+)?) (.+)")
         lock = Lock()
 
-        query = raw_input("Please input delay and text\n")
+        query = raw_input("Please input delay and text or 'exit' to exit.\n")
+        if query.lower().strip() == 'exit':
+            return
         checked_params = query_parser.search(query)
         if checked_params is None:
-            raise Exception("Bad string. string must be like '12.12 text'\n")
+            raise Exception("Bad string. string must be like '1.2 text'.\n")
         params = checked_params.groups()
         delay = float(params[0])
 
         text = params[-1]
-        # worker = partial(waiting_func, delay=delay, text=text, lock=lock)
         Process(target=waiting_func, args=(delay, text, lock)).start()
 
 
