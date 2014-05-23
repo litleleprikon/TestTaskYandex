a7600f2aeea5c420f96c2862e4b5f21b0ad55b39
litleleprikon <litleleprikon@gmail.com>
2014-05-23 13:12:57 +0400

    Release

--- lock_free_printer/first_solution.py
+++ lock_free_printer/first_solution.py
@@ -2,39 +2,68 @@
 # -*- coding: utf-8 -*-
 __author__ = 'litleleprikon'
 
+
 import re
-from multiprocessing import Process, Lock
-from time import sleep
+import sys
+import select
+import time
+
+timeout = 0.1  # seconds
+last_work_time = time.time()
 
 
-def waiting_func(delay, text, lock):
-    sleep(delay)
-    with lock:
-        print('\n{0:s}\n'.format(text))
-
-
-def main():
+def waiting_func(delay, text):
+    timeout = time.time() + delay
     while True:
-        query_parser = re.compile(r"(\d+(.\d+)?) (.+)")
-        lock = Lock()
-        with lock:
-            query = raw_input("Please input delay and text or 'exit' to exit.\n")
-        if query.lower().strip() == 'exit':
-            print('\nGoodbye!\n')
-            return
-        checked_params = query_parser.search(query)
-        if checked_params is not None:
-            params = checked_params.groups()
-            delay = float(params[0])
-
-            text = params[-1]
-            Process(target=waiting_func, args=(delay, text, lock)).start()
+        if time.time() >= timeout:
+            print(text)
+            break
         else:
-            print("Bad string. string must be like '1.2 text'.\n")
+            yield
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt as ex:
-        print("\nGoodbye!\n")
+def idle_work():
+    global last_work_time
+    now = time.time()
+    # do some other stuff every 2 seconds of idleness
+    if now - last_work_time > 2:
+        print('Idle for too long; doing some other stuff.')
+        last_work_time = now
+
+
+def main_loop():
+    query_parser = re.compile(r"(\d+(.\d+)?) (.+)")
+    read_list = [sys.stdin]
+    waitors = []
+    print("Please input delay and text or 'exit' to exit.\n")
+    while read_list:
+        ready = select.select(read_list, [], [], timeout)[0]
+        if ready:
+            for file in ready:
+                line = file.readline()
+                if not line:
+                    read_list.remove(file)
+                elif line.rstrip():
+                    if line.lower().strip() == 'exit':
+                        print('\nGoodbye!\n')
+                        return
+                    checked_params = query_parser.search(line)
+                    if checked_params is not None:
+                        params = checked_params.groups()
+                        delay = float(params[0])
+                        text = params[-1]
+                        waitors.append(waiting_func(delay, text))
+                        print("Please input delay and text or 'exit' to exit.\n")
+                    else:
+                        print("Bad string. string must be like '1.2 text'.\n")
+        for waitor in waitors:
+            try:
+                waitor.next()
+            except StopIteration:
+                pass
+
+
+try:
+    main_loop()
+except KeyboardInterrupt:
+    print('\nGoodbye!\n')
